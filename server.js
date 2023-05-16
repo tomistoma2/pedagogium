9c3e730dc8303a7a13794faedbd686344375f50e
Tomistoma <73132452+Tomistoma@users.noreply.github.com>
2023-05-16 14:30:03 +0200

    1

--- server.js
+++ server.js
@@ -27,6 +27,14 @@ const teachers = require('./routes/teachers');
 app.use('/teachers/', teachers);
 
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
 
 app.listen(3001, () => {
 console.log("Server is running")
